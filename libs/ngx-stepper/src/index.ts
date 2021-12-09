0fcb6e2ef340209c4a74a8225cf18d06c11e7991
Valentin Gavran <valentin.gavran@icloud.com>
2021-12-09 14:12:37 +0100

    Make stepper services publicly available

--- libs/ngx-stepper/src/index.ts
+++ libs/ngx-stepper/src/index.ts
@@ -3,3 +3,6 @@ export * from './lib/ngx-stepper.module';
 export * from './lib/components/stepper/stepper.component';
 export * from './lib/components/stepper-header/stepper-header.component';
 export * from './lib/components/stepper-step/stepper-step.component';
+
+export * from './lib/services/stepper/stepper.service';
+export * from './lib/services/stepper-step/stepper-step.service';
